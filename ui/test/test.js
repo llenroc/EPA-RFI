40129e8f8772bebf82fc866dd071542ed5d7d471
Michael Bowman <bowmanmc@gmail.com>
2015-12-14 11:21:26 -0500

    Add in karma and jasmine unit tests

---
+++ ui/test/test.js
@@ -0,0 +1,11 @@
+/**
+ * First test... tests that karma, jasmine, and the rest of the toolchain are
+ * setup properly and working.
+ */
+describe('Test Scaffolding', function() {
+
+    it('can execute a single unit test with no dependencies', function() {
+        expect(true).toBe(true);
+    });
+
+});
