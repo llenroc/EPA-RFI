1728a65eed9adafe4849bb4210b158b270e6bfaa
Michael Bowman <bowmanmc@gmail.com>
2015-12-14 12:56:57 -0500

    fixing jshintrc to have angular and jasmine defined globally

--- ui/test/test.js
+++ ui/test/test.js
@@ -2,6 +2,8 @@
  * First test... tests that karma, jasmine, and the rest of the toolchain are
  * setup properly and working.
  */
+'use strict';
+
 describe('Test Scaffolding', function() {
 
     it('can execute a single unit test with no dependencies', function() {
