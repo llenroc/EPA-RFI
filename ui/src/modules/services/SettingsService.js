cbfca91833e8828bfc7c19d9505219e761148722
Michael Bowman <bowmanmc@gmail.com>
2015-12-15 08:33:38 -0500

    cleaning up the geolocation interface

--- ui/src/modules/services/SettingsService.js
+++ ui/src/modules/services/SettingsService.js
@@ -14,18 +14,16 @@ module.exports = function(ngModule) {
             var timeout = 10 * 1000; // convert to seconds
 
             var gotLocation = function(position) {
-                console.log('Position: ' + position.coords.latitude + ', ' + position.coords.longitude);
                 var endTime = moment();
                 var duration = moment(endTime.diff(startTime)).format('mm:ss:SSS');
-                console.log('SettingsService.getLocation() time: ' + duration);
+                console.log('SettingsService.getLocation() took ' + duration);
                 deferred.resolve(position);
             };
             
             var noLocation = function() {
-                console.log('No location within timeout ()');
                 var endTime = moment();
                 var duration = moment(endTime.diff(startTime)).format('mm:ss:SSS');
-                console.log('SettingsService.getLocation() time: ' + duration);
+                console.log('SettingsService.getLocation() (no position) took ' + duration);
                 deferred.resolve(null);
             };
 
