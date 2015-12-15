ff6a83e7e5ebae04c56c7162ca9a7a8ebbe6559d
Michael Bowman <bowmanmc@gmail.com>
2015-12-15 14:25:56 -0500

    default api endpoint (configurable version coming soon)

--- ui/src/modules/services/AirQualityService.js
+++ ui/src/modules/services/AirQualityService.js
@@ -8,7 +8,7 @@ module.exports = function(ngModule) {
 
         this.updateAirQuality = function(latitude, longitude) {
             var distance = 100;
-            var url = `/api/air-quality/lat-lon/${distance}/${latitude}/${longitude}`;
+            var url = `http://epa-server.cfapps.io/api/air-quality/lat-lon/${distance}/${latitude}/${longitude}`;
             var svc = this;
             $http.get(url).then(function(response) {
                 console.log('Got an api response: ' + JSON.stringify(response));
