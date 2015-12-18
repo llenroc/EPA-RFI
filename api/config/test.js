c6dc194979ea4e49d435c4d22dd45826c252337f
Jonathan Meyer <jon@gisjedi.com>
2015-12-18 13:04:19 -0500

    Added unit test runner for Travis

--- api/config/test.js
+++ api/config/test.js
@@ -1,3 +1,11 @@
 'use strict';
 
-// nothing here
+module.exports = {
+    postgresDb: {
+        hostName: 'localhost',
+        database: 'postgres',
+        user: 'postgres',
+        pass: '',
+        port: 5432
+    }
+};
