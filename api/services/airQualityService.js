b38379777bc44e62320b32facc324fda88cde55a
Jonathan Meyer <jon@gisjedi.com>
2015-12-18 15:56:55 -0500

    Reverting templating

--- api/services/airQualityService.js
+++ api/services/airQualityService.js
@@ -48,8 +48,10 @@ function AirQualityService() {
     pub.getAirQualityByLatLon = function(distance, lat, lon, callback) {
         var format = 'application/json';
 
-        var apiUrl = `${config.epaApi.baseUrl}/aq/observation/latLong/current/?`
-            `API_KEY=${config.epaApi.apiKey}&latitude=${lat}&longitude=${lon}&distance=${distance}&format=${format}`;
+        var apiUrl = config.epaApi.baseUrl + '/aq/observation/latLong/current/?API_KEY='
+            + config.epaApi.apiKey +
+            '&latitude=' + lat + '&longitude=' + lon +
+            '&distance=' + distance + '&format=' + format;
         request(apiUrl, function(error, response, bodyRaw) {
             extractAirQuality(error, response, bodyRaw, callback);
         });
@@ -59,8 +61,9 @@ function AirQualityService() {
         var format = 'application/json';
 
 
-        var apiUrl = `${config.epaApi.baseUrl}/aq/observation/zipCode/current/`
-            `?API_KEY=${config.epaApi.apiKey}&zipCode=${zipCode}&distance=${distance}&format=${format}`;
+        var apiUrl = config.epaApi.baseUrl + '/aq/observation/zipCode/current/?API_KEY='
+            + config.epaApi.apiKey +
+            '&zipCode=' + zipCode + '&distance=' + distance + '&format=' + format;
         request(apiUrl, function(error, response, bodyRaw) {
             extractAirQuality(error, response, bodyRaw, callback);
         });
